b86f33825698293fc7a7426799fd0c9f6fefa6ed
nikoleesg <nikoleesg@users.noreply.github.com>
2023-11-05 13:32:58 +0000

    Fix styling

--- src/Enums/SurveyTypeEnum.php
+++ src/Enums/SurveyTypeEnum.php
@@ -6,16 +6,15 @@ enum SurveyTypeEnum: string
 {
     case Online = 'OnlineBasic';
     case FreeIntercept = 'Basic';
-//    case JointTargets = 'Basic';
-//    case IndividualTargets = 'Basic';
+    //    case JointTargets = 'Basic';
+    //    case IndividualTargets = 'Basic';
     case SamplingPointsWithQuota = 'Advanced';
     case SamplingPointsWithAddresses = 'EuroBarometer';
     case SamplingPointsWithQuotaAndQuota = 'EuroBarometerAdvanced';
 
     public function channel(): string
     {
-        return match($this)
-        {
+        return match ($this) {
             SurveyTypeEnum::Online => 'Online',
             SurveyTypeEnum::FreeIntercept => 'CAPI',
             SurveyTypeEnum::JointTargets => 'CAPI',
