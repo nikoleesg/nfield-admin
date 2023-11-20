651490dd6c12638e4290bf97aee14330017e61b0
Niko Lee <Niko.lee@outlook.sg>
2023-11-20 21:22:29 +0800

    add QuotaVersionsEndpoint

--- src/Endpoints/v1/QuotaVersionsEndpoint.php
+++ src/Endpoints/v1/QuotaVersionsEndpoint.php
@@ -4,7 +4,31 @@
 namespace Nikoleesg\NfieldAdmin\Endpoints\v1;
 
 
+use Illuminate\Support\Str;
+use Spatie\LaravelData\DataCollection;
+use Nikoleesg\NfieldAdmin\HttpClient;
+
 class QuotaVersionsEndpoint
 {
+    protected string $resourcePath = 'v1/Surveys/{surveyId}/QuotaVersions';
 
+    protected HttpClient $httpClient;
+
+    public function __construct(string $surveyId)
+    {
+        $this->resourcePath = Str::replace('{surveyId}', $surveyId, $this->resourcePath);
+
+        $this->httpClient = new HttpClient();
+    }
+
+    public function index()
+    {
+        $resourcePath = $this->resourcePath;
+
+        $response = $this->httpClient->get($resourcePath);
+
+        $quotaVersions = json_decode($response->body(), true);
+
+        return $resourcePath;
+    }
 }
