316c534c162770ac8227e85e50abd093ea75cd75
Niko Lee <Niko.lee@outlook.sg>
2023-11-20 12:21:19 +0800

    add SamplingPoints Endpoint

--- src/Endpoints/v1/SamplingPointsEndpoint.php
+++ src/Endpoints/v1/SamplingPointsEndpoint.php
@@ -5,8 +5,11 @@ namespace Nikoleesg\NfieldAdmin\Endpoints\v1;
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
+use Spatie\LaravelData\DataCollection;
+use Illuminate\Http\Client\Response;
 use Nikoleesg\NfieldAdmin\HttpClient;
 use Nikoleesg\NfieldAdmin\Data\SamplingPointData;
+use Spatie\LaravelData\Exceptions\CannotCreateData;
 
 class SamplingPointsEndpoint
 {
@@ -23,58 +26,103 @@ class SamplingPointsEndpoint
 
     /**
      * Get a list of all sampling points for a survey.
-     * @return Collection
+     *
+     * @return DataCollection
      */
-    public function getSamplingPoints(): Collection
+    public function index(): DataCollection
     {
-        $response = $this->httpClient->get($this->resourcePath);
+        $resourcePath = $this->resourcePath;
+
+        $response = $this->httpClient->get($resourcePath);
 
         $samplingPoints = json_decode($response->body(), true);
 
-        $samplingPointsCollection = collect();
-
-        foreach ($samplingPoints as $samplingPoint)
-        {
-            $samplingPointsCollection->push(SamplingPointData::from($samplingPoint));
-        }
-
-        return $samplingPointsCollection;
+        return SamplingPointData::collection($samplingPoints);
     }
 
     /**
      * Get the details of a specific sampling point.
+     *
      * @param string $samplingPointId
      * @return SamplingPointData
      */
-    public function getSamplingPoint(string $samplingPointId): SamplingPointData
+    public function show(string $samplingPointId): SamplingPointData
     {
-        $response = $this->httpClient->get($this->resourcePath . '/' . $samplingPointId);
+        $resourcePath = $this->resourcePath . "/$samplingPointId";
+
+        $response = $this->httpClient->get($resourcePath);
 
         $samplingPoint = json_decode($response->body(), true);
 
-        return SamplingPointData::from($samplingPoint);
+        try {
+            $samplingPointData = SamplingPointData::from($samplingPoint);
+
+        } catch (CannotCreateData $exception) {
+            $samplingPointData = SamplingPointData::optional(null);
+        }
+
+        return $samplingPointData;
     }
 
     /**
-     * @param SamplingPointData $samplingPointData
+     * Delete a specified sampling point.
+     *
+     * @param string $samplingPointId
+     * @return bool
      */
-    public function createSamplingPoint(SamplingPointData $samplingPointData): SamplingPointData
+    public function destroy(string $samplingPointId): bool
     {
-        $response = $this->httpClient->post($this->resourcePath, true, $samplingPointData->toArray());
+        $resourcePath = $this->resourcePath . "/$samplingPointId";
+
+        $response = $this->httpClient->delete($resourcePath);
+
+        return empty($response->body());
+    }
+
+    public function update()
+    {
+
+    }
+
+    /**
+     * Create a new sampling point.
+     *
+     * @param SamplingPointData $samplingPointData
+     * @return SamplingPointData
+     */
+    public function store(SamplingPointData $samplingPointData): SamplingPointData
+    {
+        $resourcePath = $this->resourcePath;
+
+        // Transform to Nfield API SurveyModel
+        $samplingPointCreationModel = array_change_key_casing($samplingPointData->toArray(), CASE_STUDLY);
+
+        $response = $this->httpClient->post($resourcePath, true, $samplingPointCreationModel);
 
         $samplingPoint = json_decode($response->body(), true);
 
-        return SamplingPointData::from($samplingPoint);
+        try {
+            $samplingPointData = SamplingPointData::from($samplingPoint);
+        } catch (CannotCreateData $exception) {
+            $samplingPointData = SamplingPointData::optional(null);
+        }
+
+        return $samplingPointData;
     }
 
     /**
      * Returns the number of samplingPoints of the survey.
+     *
      * @return int
      */
-    public function getCount(): int
+    public function count(): int
     {
-        $response = $this->httpClient->get($this->resourcePath . '/Count');
+        $resourcePath = $this->resourcePath . "/Count";
 
-        return $response->body();
+        $response = $this->httpClient->get($resourcePath);
+
+        $samplingPointCnt = json_decode($response->body(), true);
+
+        return $samplingPointCnt;
     }
 }
