70fe202f0689449fb5e0a7c1bcf1a38fa357b0be
Niko Lee <Niko.lee@outlook.sg>
2023-11-21 13:25:58 +0800

    add Interviewer assignments

--- src/Services/InterviewerService.php
+++ src/Services/InterviewerService.php
@@ -6,23 +6,43 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Nikoleesg\NfieldAdmin\Data\NewCapiInterviewerRequestData;
 use Spatie\LaravelData\DataCollection;
-use Nikoleesg\NfieldAdmin\Endpoints\v1\CapiInterviewersEndpoint;
+use Nikoleesg\NfieldAdmin\Endpoints\v1;
 use Nikoleesg\NfieldAdmin\Data\InterviewerData;
 
 class InterviewerService
 {
-    protected CapiInterviewersEndpoint $capiInterviewerEndpoint;
+    protected ?string $interviewerId;
+
+    protected v1\CapiInterviewersEndpoint $capiInterviewerEndpoint;
+
+    protected v1\InterviewerAssignmentsEndpoint $interviewerAssignmentsEndpoint;
 
     public function __construct()
     {
-        $this->initEndpoint();
+        $this->initEndpoints();
     }
 
-    protected function initEndpoint(): self
+    protected function initEndpoints(): self
     {
-        $this->capiInterviewerEndpoint = new CapiInterviewersEndpoint();
+        $this->capiInterviewerEndpoint = new v1\CapiInterviewersEndpoint();
+
+        if ($this->isInterviewerConfigured()) {
+            $this->interviewerAssignmentsEndpoint = new v1\InterviewerAssignmentsEndpoint($this->interviewerId);
+        }
 
         return $this;
+    }
+
+    protected function isInterviewerConfigured(): bool
+    {
+        return isset($this->interviewerId);
+    }
+
+    public function setInterviewer(string $interviewerId): self
+    {
+        $this->interviewerId = $interviewerId;
+
+        return $this->initEndpoints();
     }
 
     public function getCapiInterviewers(): DataCollection
@@ -60,6 +80,11 @@ class InterviewerService
         }
 
         return $this->capiInterviewerEndpoint->update($interviewerId, $password);
+    }
+
+    public function getAssignments(): DataCollection
+    {
+        return $this->interviewerAssignmentsEndpoint->index();
     }
 
 }
