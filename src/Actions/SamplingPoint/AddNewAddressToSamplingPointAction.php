5cb166ada17e0d121383401e46b14f9a27c2f400
Niko Lee <Niko.lee@outlook.sg>
2023-11-24 16:13:06 +0800

    add actions

---
+++ src/Actions/SamplingPoint/AddNewAddressToSamplingPointAction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Nikoleesg\NfieldAdmin\Actions\SamplingPoint;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+use Nikoleesg\NfieldAdmin\Facades\SamplingPoint;
+use Nikoleesg\NfieldAdmin\Data\AddressDTO;
+
+class AddNewAddressToSamplingPointAction
+{
+    use AsAction;
+
+    /**
+     * Handle the action.
+     * @param string $surveyId
+     * @param string $samplingPointId
+     * @param AddressDTO $addressDTO
+     */
+    public function handle(string $surveyId, string $samplingPointId, AddressDTO $addressDTO)
+    {
+        SamplingPoint::setSurvey($surveyId)->setSamplingPoint($samplingPointId)->createAddress($addressDTO);
+    }
+}
