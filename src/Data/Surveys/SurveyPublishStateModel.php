9ab1a4b44a52b1a77a4893a6cd10df846fb47962
Niko Lee <niko.lee@outlook.sg>
2026-04-26 00:05:56 +0800

    Fix SurveyPublishStateModel to map PascalCase API response keys
    Add MapInputName attribute with StudlyCaseMapper to properly map
    "Live" and "Test" keys from NField API to PHP properties.
    
    This fixes the property name mismatch where API returns PascalCase
    but DTO expected lowercase property names.

--- src/Data/Surveys/SurveyPublishStateModel.php
+++ src/Data/Surveys/SurveyPublishStateModel.php
@@ -3,10 +3,13 @@
 namespace Nikoleesg\NfieldAdmin\Data\Surveys;
 
 use Nikoleesg\NfieldAdmin\Enums\SurveyPublishStateEnum;
+use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Attributes\WithCast;
 use Spatie\LaravelData\Casts\EnumCast;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Mappers\StudlyCaseMapper;
 
+#[MapInputName(StudlyCaseMapper::class)]
 class SurveyPublishStateModel extends Data
 {
     public function __construct(
